9276d55213906e2735b90e626d49a568e8188175
Samuel Persian <samuel.persian@gmail.com>
2017-03-07 21:30:15 -0700

    pi test passing

---
+++ src/main/java/com/example/PagesController.java
@@ -0,0 +1,15 @@
+package com.example;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class PagesController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello World";
+    }
+}
